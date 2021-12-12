c921ec583dfe651ab80d4d7345d35636b26f0a20
Seden0 <94846806+Seden0@users.noreply.github.com>
2021-12-11 21:06:44 -0600

    newpull

--- src/components/Admin/ListOrdes.js
+++ src/components/Admin/ListOrdes.js
@@ -10,37 +10,92 @@ import { onValue, ref, remove } from "@firebase/database";
 import { database } from "../../config/firebaseConfig";
 import { useState, useEffect } from "react";
 import IconButton from "@mui/material/IconButton";
+//
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import Badge from "@mui/material/Badge";
+import Container from "@mui/material/Container";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { menuItems } from "./menu2";
+//
+const drawerWidth = 240;
 
-const deleteCustomer = (id) => {
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open"
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open"
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9)
+      }
+    })
+  }
+}));
+
+const mdTheme = createTheme();
+
+//
+const deleteOrder = (id) => {
   remove(ref(database, `/orders/${id}`)).then(() => {
-    alert("cliente eliminado");
+    alert("orden eliminada");
   });
 };
 
 const columns = [
   {
-    field: "fullName",
-    headerName: "Nombre",
-    sortable: false,
-    width: 250,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, "name") || ""} ${
-        params.getValue(params.id, "lastName") || ""
-      }`
-  },
-  {
-    field: "email",
-    headerName: "Email",
+    field: "id",
+    headerName: "Id",
     width: 220
   },
   {
-    field: "phone",
-    headerName: "Telefono",
+    field: "description",
+    headerName: "Descripcion",
     width: 220
   },
   {
-    field: "company",
-    headerName: "Empresa",
+    field: "total",
+    headerName: "Total",
     width: 220
   },
   {
@@ -50,7 +105,7 @@ const columns = [
     renderCell: (data) => (
       <IconButton
         onClick={() => {
-          deleteCustomer(data.row.id);
+          deleteOrder(data.row.id);
         }}
         color="primary"
         aria-label="Eliminar"
@@ -62,24 +117,29 @@ const columns = [
   }
 ];
 
-const Customers = () => {
-  const [customers, setCustomers] = useState([]);
+const Order = (props) => {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     let isMounted = true;
     onValue(
-      ref(database, "customers/"),
+      ref(database, "orders/"),
       (snapshot) => {
-        const customersList = [];
+        const ordersList = [];
 
         snapshot.forEach((item) => {
-          const customerItem = {
+          const orderItem = {
             id: item.key,
             ...item.val()
           };
-          customersList.push(customerItem);
+          ordersList.push(orderItem);
         });
-        setCustomers(customersList);
+        setOrders(ordersList);
       },
       (error) => {
         console.log(error);
@@ -92,38 +152,101 @@ const Customers = () => {
   }, []);
 
   return (
-    <Paper
-      sax={{
-        p: 3
-      }}
-    >
-      <Grid container spacing={3}>
-        <Grid item xs={10}>
-          <h3 sx={{ n: 0 }}>Clientes</h3>
-        </Grid>
-        <Grid item xs={2}>
-          <Button
-            variant="outlined"
-            LinkComponent={Link}
-            to="/clientes/agregar"
-            startIcon={<AddIcon />}
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: "24px" // keep right padding when drawer closed
+            }}
           >
-            Agregar
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <div style={{ height: 400, width: "100%" }}>
-            <DataGrid
-              rows={customers}
-              columns={columns}
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-            />
-          </div>
-        </Grid>
-      </Grid>
-    </Paper>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" })
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Pharmacy Orders
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1]
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List>{menuItems}</List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto"
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            {props.children}
+            <Paper
+              sax={{
+                p: 3
+              }}
+            >
+              <Grid container spacing={3}>
+                <Grid item xs={10}>
+                  <h3 sx={{ n: 0 }}>Ordenes</h3>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <div style={{ height: 400, width: "100%" }}>
+                    <DataGrid
+                      rows={orders}
+                      columns={columns}
+                      pageSize={5}
+                      rowsPerPageOptions={[5]}
+                    />
+                  </div>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 };
 
-export default Customers;
+export default Order;
